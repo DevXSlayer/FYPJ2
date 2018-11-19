fff10c8cb53e2828cbc095a717d70635186f4e0a
Loo Chuan Xu <lchuanx@gmail.com>
2018-11-19 13:12:52 +0800

    Partial gamepaly
    -Made action bar fill
    -Click on player to open actions
    -Time stops when character is selected
    -Select again to continue

---
+++ FYPJ2/Assets/Scripts/ActionsCreation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public struct Skills
+{
+    public Sprite skillIcon;
+    public float skillCost;
+    public string skillDesc;
+}
+
+
+public class ActionsCreation : MonoBehaviour {
+
+    //Assign the list of character skills to this list
+    private List<Skills> charSkills;
+
+    [SerializeField]
+    private GameObject samepleButton;
+
+    [SerializeField]
+    private Sprite image;
+
+    void Awake()
+    {
+        charSkills = new List<Skills>();
+        //Replace with actual list of character skills
+        for (int i = 0; i < 4; ++i)// for testing *to remove*
+        {
+            Skills temp;
+            temp.skillCost = 1.0f;
+            temp.skillDesc = "";
+            temp.skillIcon = image;
+            charSkills.Add(temp);
+        }
+
+
+
+        for (int index = 0; index < charSkills.Count; ++index)
+        {
+            //Instantiate skill button 
+            GameObject skillButton = Instantiate(samepleButton, transform);
+            skillButton.GetComponent<Image>().sprite = charSkills[index].skillIcon; 
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FYPJ2/Assets/Scripts/PlayerBattle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerBattle : MonoBehaviour {
+
+    //Character _character //Have a class to take in player properties
+
+    [SerializeField]
+    private Slider actionBar;
+    [SerializeField]
+    private GameObject actionsList;
+
+    //Variables used by this class itself
+    private bool charSelected = false;
+
+    //Values that should take from the character 
+    private float maxActionPoint = 1.0f;
+    private float actionFillRate = 0.2f;
+
+    void Awake()
+    {
+        //actionBar.value = 1.0f;  //maxActionPoint
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (actionBar.value < maxActionPoint)
+            actionBar.value += actionFillRate * Time.deltaTime;
+        
+        if(charSelected)
+        {
+            // TO do make selection bar appear
+            actionsList.SetActive(true);
+        }   
+        else
+        {
+            actionsList.SetActive(false);
+        }
+	}
+
+    public void OnCharacterSelect()
+    {
+        //When player selects character for an action, etc
+        if (!charSelected)  
+        {
+            charSelected = true;
+            Time.timeScale = 0.0f;
+        }
+        //When player decides to cancel action selection
+        else
+        {
+            charSelected = false;
+            Time.timeScale = 1.0f;
+        }
+    }
+}
