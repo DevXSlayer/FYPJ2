1487e6656621c8432e5b649998dceb657dde31ab
Shadownod <shadow_nod@hotmail.com>
2019-01-15 09:45:46 +0800

    lol
    lol

---
+++ FYPJ2/Assets/Scripts/ScriptableObjects/MakeWeaponObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MakeWeaponObject{
+
+    [MenuItem("Assets/Create/WeaponObject")]
+    public static void Create()
+    {
+        WeaponObject asset = ScriptableObject.CreateInstance<WeaponObject>();
+        AssetDatabase.CreateAsset(asset, "Assets/Scripts/ScriptableObjects/WeaponFiles/NewWeapon.asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
---
+++ FYPJ2/Assets/Scripts/ScriptableObjects/WeaponObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WeaponObject : ScriptableObject {
+
+    public string weaponName = "Weapon Name Here";
+    public int cost = 50;
+    public string description;
+
+    public float damage = 0;
+
+}
