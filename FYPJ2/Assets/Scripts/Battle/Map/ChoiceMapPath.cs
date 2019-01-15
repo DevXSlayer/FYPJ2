5be09b60b492d27503f9dbe9e90f1bf9d5b02401
Chuan Xu <lchuanx@gmail.com>
2019-01-15 10:35:15 +0800

    Making changes to prefabs and organizing them

--- FYPJ2/Assets/Scripts/Battle/Map/ChoiceMapPath.cs
+++ FYPJ2/Assets/Scripts/Battle/Map/ChoiceMapPath.cs
@@ -12,16 +12,32 @@ public class ChoiceMapPath : MonoBehaviour {
     [SerializeField]
     private GameObject DirectionArrow;
 
+    [SerializeField]
+    private GameObject Enemy;
+
+    [SerializeField]
+    private GameObject Item;
+
+    private int PathNumber = 0;
+
     private void Start()
     {
-        for (int PathIndex = 0; PathIndex < Paths.Length; ++PathIndex)
+        int SpawnFactor= Random.Range(0, 2);
+        switch (SpawnFactor)
         {
-            Vector2 Dir =(Paths[PathIndex].transform.position - transform.position).normalized;
-            float Angle = Mathf.Atan2(Paths[PathIndex].transform.position.y - transform.position.y, Paths[PathIndex].transform.position.x - transform.position.x) * 180.0f / Mathf.PI;
-            GameObject DirChoices = Instantiate(DirectionArrow,new Vector3(Mathf.Clamp(Dir.x,-0.5f,0.5f),Mathf.Clamp(Dir.y,-0.5f,0.5f),0),Quaternion.Euler(0,0,Angle), transform);
-            DirChoices.GetComponent<Image>().alphaHitTestMinimumThreshold = 1.0f;
-            DirChoices.GetComponent<Button>().onClick.AddListener(Pressed);
+            case 0:
+                Instantiate(Enemy, transform);
+                break;
+            case 1:
+                Instantiate(Item, transform);
+                break;
         }
+    }
+
+    private void Update()
+    {
+        if (CheckPlayerDistance())
+            SpawnDirArrows();
     }
 
     private void OnDrawGizmosSelected()
@@ -39,8 +55,27 @@ public class ChoiceMapPath : MonoBehaviour {
         }
     }
 
-    void Pressed()
+    void Pressed(int PathIndex)
     {
-        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        //Set position of the player to the point
+        //Paths[PathIndex].transform.position;
+    }
+
+    bool CheckPlayerDistance()
+    {
+        return false;
+    }
+
+    void SpawnDirArrows()
+    {
+        for (int PathIndex = 0; PathIndex < Paths.Length; ++PathIndex)
+        {
+            Vector2 Dir = (Paths[PathIndex].transform.position - transform.position).normalized;
+            float Angle = Mathf.Atan2(Paths[PathIndex].transform.position.y - transform.position.y, Paths[PathIndex].transform.position.x - transform.position.x) * 180.0f / Mathf.PI;
+            GameObject DirChoices = Instantiate(DirectionArrow, new Vector3(Mathf.Clamp(Dir.x, -0.5f, 0.5f) + transform.position.x, Mathf.Clamp(Dir.y, -0.5f, 0.5f) + transform.position.y, 0), Quaternion.Euler(0, 0, Angle), transform.GetChild(0));
+            DirChoices.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+            DirChoices.GetComponent<Button>().onClick.AddListener(() => Pressed(PathNumber));
+            ++PathNumber;
+        }
     }
 }
--- FYPJ2/Assets/Scripts/Handlers/Enemy Randomizer/SlimeRandomizer.cs
+++ FYPJ2/Assets/Scripts/Handlers/Enemy Randomizer/SlimeRandomizer.cs
@@ -27,21 +27,37 @@ public class SlimeRandomizer : MonoBehaviour {
     private GameObject EnemyPrefab;
 
     SLIMETYPES[] EnemyTypes;
-	// Use this for initialization
-	void Start () {
+
+    private void Update()
+    {
+        if (CheckPlayerDistance())
+        {
+            SpawnEnemies();
+        }
+    }
+
+    bool CheckPlayerDistance()
+    {
+        return false;
+    }
+
+    void SpawnEnemies()
+    {
+        if (!BattleCanvas.activeSelf)
+            BattleCanvas.SetActive(true);
+
         int NoOfEnemies = Random.Range(1, MaxNoEnemies + 1);
 
         EnemyTypes = new SLIMETYPES[NoOfEnemies];
 
-        Debug.Log(NoOfEnemies);
-        for(int EnemyIndex = 0; EnemyIndex < NoOfEnemies; ++EnemyIndex)
+        for (int EnemyIndex = 0; EnemyIndex < NoOfEnemies; ++EnemyIndex)
         {
             EnemyTypes[EnemyIndex] = (SLIMETYPES)(Random.Range(0, (int)SLIMETYPES.TOTAL));
         }
-        
-        for(int InitIndex = 0; InitIndex < EnemyTypes.Length; ++InitIndex)
+
+        for (int InitIndex = 0; InitIndex < EnemyTypes.Length; ++InitIndex)
         {
-            GameObject Enemy = GameObject.Instantiate(EnemyPrefab,BattleCanvas.transform.GetChild(InitIndex));
+            GameObject Enemy = GameObject.Instantiate(EnemyPrefab, BattleCanvas.transform.GetChild(InitIndex));
             Enemy.GetComponent<Image>().sprite = SlimeSprites[(int)EnemyTypes[InitIndex]];
             Enemy.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = SlimeSprites[(int)EnemyTypes[InitIndex]].name;
 
