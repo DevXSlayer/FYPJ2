5537edcc5dc508ded4bb857714bc67c5c534113f
Shadownod <shadow_nod@hotmail.com>
2019-01-29 11:08:16 +0800

    lol
    lol

---
+++ FYPJ2/Assets/Scripts/TavernButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TavernButton : MonoBehaviour {
+
+    public int weaponNumber;
+
+    public PlayerVars player;
+
+    public ShopObject shop;
+
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI cost;
+    public TextMeshProUGUI description;
+
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        SetButton();
+
+        //player = player.GetComponent<PlayerVars>();
+    }
+
+    void SetButton()
+    {
+        string costString = shop.ShopList[weaponNumber].cost.ToString();
+        name.text = shop.ShopList[weaponNumber].name;
+        cost.text = "$" + shop.ShopList[weaponNumber].cost;
+        description.text = shop.ShopList[weaponNumber].description;
+    }
+
+    public void OnClick()
+    {
+        if (player.gold >= shop.ShopList[weaponNumber].cost)
+        {
+
+            player.gold -= shop.ShopList[weaponNumber].cost;
+
+        }
+        else
+        {
+            source.Play();
+        }
+    }
+
+}
