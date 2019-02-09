1134c1ce53a6e3db7cc59b5e88407851545ad7e0
Loo Chuan Xu <lchuanx@gmail.com>
2019-02-09 19:33:05 +0800

    more things

---
+++ FYPJ2/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager Instance;
+    private AudioSource GameAudio;
+
+    private void Awake()
+    {
+        Instance = this;
+        GameAudio = GetComponent<AudioSource>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
